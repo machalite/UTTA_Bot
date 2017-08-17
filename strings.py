8240ec81cd7775481d53d5eacdb7b5f6f1dd5d01
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-08-18 02:45:08 +0700

    Added list of command on !help

--- strings.py
+++ strings.py
@@ -1,3 +1,16 @@
 class Strings:
 	def __init__(self):
-		self.HELP = "Ini pertolongan."
+        # strings to be displayed when user request !help
+		self.HELP = " \
+            \n[~] List Of Commands: \n \
+            \n[1]. !register <student pin> \
+            \n[2]. !today \
+            \n[3]. !roomcheck <room id/room name>\
+            \n[4]. !schedule \
+            \n[5]. !next \
+            \n[6]. !where <room name/building name> \
+            \n[7]. !check <course name/course ID> \
+            \n[8]. !changes \n \
+            \n[9]. !translate <text> \
+            \n[10]. !search <kata kunci> \
+            \n[11]. !help \"
