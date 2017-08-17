3894a11c9e6677c393dcc98bc8ff24da648bb7f3
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-08-18 01:38:47 +0700

    Fixed missing bracket

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, buttons_template)
+    )
 
 
 if __name__ == '__main__':
