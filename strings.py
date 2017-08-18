8aee058bede4733654d462cfff8b8d7f6c49e868
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-08-18 08:54:17 +0700

    asdf

--- strings.py
+++ strings.py
@@ -15,6 +15,6 @@ class Strings:
         # error messages
         self.ERR_PC = "This command is incompatible on LINE PC"
         # URLs
-        self.IMGUR_UTTA = 'http://imgur.com/Hn0d1DJ'
+        self.IMGUR_UTTA = "http://imgur.com/Hn0d1DJ"
         # long strings
         self.ABOUT = "UNIVERSITY TIME TABLE BOT\nCreated by Jonathan"
