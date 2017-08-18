61a51ebbaf6076a3ae862005071ae2e9d85105ea
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-08-18 08:39:56 +0700

    Changed image URL

--- strings.py
+++ strings.py
@@ -15,7 +15,7 @@ class Strings:
         # error messages
         self.ERR_PC = "This command is incompatible on LINE PC"
         # URLs
-        self.IMGUR_UTTA = "https://i.imgur.com/qKkE2bj.jpg"
+        self.IMGUR_UTTA = "http://imgur.com/Hn0d1DJ"
         # long strings
         self.ABOUT = "UNIVERSITY TIME TABLE BOT\n \
         Created by Jonathan 2013042032"
