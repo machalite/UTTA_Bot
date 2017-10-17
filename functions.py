c56cfc39ac064e6a3d0924504ea78d032075dd3e
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-10-17 15:27:55 +0700

    removed global vars

--- functions.py
+++ functions.py
@@ -3,15 +3,14 @@ from settings import *
 
 import MySQLdb
 
-# create DB connection
-con = MySQLdb.connect(Settings().DBhost, Settings().DBuser,
-                      Settings().DBpass, Settings().DBname)
-
-# create cursor for query data execution
-cur = con.cursor()
-
 
 def selUser():
+    # create DB connection
+    con = MySQLdb.connect(Settings().DBhost, Settings().DBuser,
+                          Settings().DBpass, Settings().DBname)
+
+    # create cursor for query data execution
+    cur = con.cursor()
     # Use all the SQL you like
     cur.execute("SELECT * FROM faculty")
 
@@ -25,6 +24,12 @@ def selUser():
 
 
 def today():
+    # create DB connection
+    con = MySQLdb.connect(Settings().DBhost, Settings().DBuser,
+                          Settings().DBpass, Settings().DBname)
+
+    # create cursor for query data execution
+    cur = con.cursor()
     # SQL statement
     qry = "SELECT cr.name, cr.code, c.startclass, c.endclass, r.name AS building FROM takencourse t, course cr, class c, room r WHERE t.course=cr.id AND c.course=cr.id AND c.room=r.id AND c.day=1 AND c.active=1 AND t.student=1 ORDER BY c.startclass"
 
