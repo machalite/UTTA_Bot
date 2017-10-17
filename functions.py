10f986febb4a0669232ca642f8c2e3f22464da93
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-10-17 13:18:55 +0700

    added today()

--- functions.py
+++ functions.py
@@ -4,22 +4,45 @@ from settings import *
 import MySQLdb
 
 
-def selUser():
-    db = MySQLdb.connect(host=Settings().DBhost,
-                         user=Settings().DBuser,
-                         passwd=Settings().DBpass,
-                         db=Settings().DBname)
+class Functions:
+    def __init__(self):
+        # create DB connection
+        self.db = MySQLdb.connect(Settings().DBhost, Settings().DBuser,
+                                  Settings().DBpass, Settings().DBname)
+        # create cursor for query data execution
+        self.cur = db.cursor()
 
-    # you must create a Cursor object. It will let
-    #  you execute all the queries you need
-    cur = db.cursor()
+    def selUser():
+        # Use all the SQL you like
+        cur.execute("SELECT * FROM faculty")
 
-    # Use all the SQL you like
-    cur.execute("SELECT * FROM faculty")
+        # print all the first cell of all the rows
+        for row in cur.fetchall():
+            content = row[0]
 
-    # print all the first cell of all the rows
-    for row in cur.fetchall():
-        content = row[0]
+        db.close()
+        return content
 
-    db.close()
-    return content
+    def today():
+        # SQL statement
+        qry = "SELECT cr.name, cr.code, c.startclass, c.endclass,
+        r.name AS building
+        FROM takencourse t, course cr, class c, room r
+        WHERE t.course=cr.id
+        AND c.course=cr.id
+        AND c.room=r.id
+        AND c.day=today
+        AND c.active=1
+        AND t.student=studentId
+        ORDER BY c.startclass"
+
+        # Use all the SQL you like
+        cur.execute(qry)
+
+        # print all the first cell of all the rows
+        result="Today's schedule:\n"
+        for row in cur.fetchall():
+            result + = row[0] + row[1] + row[2] + row[3] + row[4] + "\n"
+
+        db.close()
+        return result
--- settings.py
+++ settings.py
@@ -6,9 +6,3 @@ class Settings:
         self.DBuser = "bfc1e725797bdf"
         self.DBpass = "c4e344fc"
         self.DBname = "heroku_2ad6e3d7ef27f71"
-
-        # DB connection
-        # self.DBhost = "atmajaya.org"
-        # self.DBuser = "atmajaya_utta"
-        # self.DBpass = "=+6gPpL+GUfK"
-        # self.DBname = "atmajaya_uttadb"
