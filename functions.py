93e2c9be161579bc0594f18db2e4bc1b7ccd8c8a
MACHALITE\Machalite <jonathanhuang7070@gmail.com>
2017-10-18 03:23:07 +0700

    added verify()

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from settings import *
+from strings import *
 
 import MySQLdb
 
@@ -32,7 +33,8 @@ def today():
 
     # create cursor for query data execution
     cur = con.cursor()
-    # SQL statement
+
+    # fetch today's classes
     qry = "SELECT cr.name, cr.code, c.startclass, c.endclass, r.name AS building FROM takencourse t, course cr, class c, room r WHERE t.course=cr.id AND c.course=cr.id AND c.room=r.id AND c.day=1 AND c.active=1 AND t.student=1 ORDER BY c.startclass"
 
     # Use all the SQL you like
@@ -41,10 +43,42 @@ def today():
     # print all the first cell of all the rows
     result = "Today's schedule:\n"
 
+    # arranging query so it displayed nicely
     for row in cur.fetchall():
         result += str(row[1])+" "+str(row[0])+"\n"
         result += str(row[2])+" - "+str(row[3])+"\n"
         result += str(row[4])+"\n\n"
+
+    # close connection
+    con.close()
+    return result
+
+
+def verify(id):
+    # create DB connection
+    con = MySQLdb.connect(Settings().DBhost, Settings().DBuser,
+                          Settings().DBpass, Settings().DBname)
+
+    # create cursor for query data execution
+    cur = con.cursor()
+
+    # Get student corresponding to the submitted line id
+    qry = "SELECT id, name FROM student WHERE lineid=" + id
+
+    cur.execute(qry)
+    # contain fetch result in array variable
+    row = cur.fetchall()
+
+    # check if there is a student with matching lineid
+    if len(row) == 0:
+        # no matching lineid
+        result = 0
+    elif len(row) == 1:
+        # there is 1 match, get the student id
+        result = row[0]
+    else:
+        # other errors
+        result = -1
 
     # close connection
     con.close()
--- strings.py
+++ strings.py
@@ -16,18 +16,18 @@ class Strings:
         self.INST_REGISTER = "Register your student ID for personalised time table.\nExample !register 2013042032"
         # error messages
         self.ERR_PC = "This command is incompatible on LINE PC"
-        self.ERR_FATAL="Failed"
+        self.ERR_FATAL = "Failed"
         # URLs
         self.IMGUR_UTTA = "http://imgur.com/Hn0d1DJ"
         # long strings
         self.ABOUT = "UNIVERSITY TIME TABLE BOT\nCreated by Jonathan"
         # messages on !register
-        self.REG_INVALID="Invalid PIN! Please try again or contact administrator for more information."
-        self.REG_EXPIRED="PIN already used! Please try again or contact administrator for more information."
-        self.REG_FAILED="Registration failed! Please contact administrator for assistance"
+        self.UNREG = "You are not registered. Please register using !register."
+        self.REG_INVALID = "Invalid PIN! Please try again or contact administrator for more information."
+        self.REG_EXPIRED = "PIN already used! Please try again or contact administrator for more information."
+        self.REG_FAILED = "Registration failed! Please contact administrator for assistance"
         # messages on !today
-        self.TODAY_UNREG="You are not registered. Please register using !register."
-        self.TODAY_EMPTY="There is no class for today"
+        self.TODAY_EMPTY = "There is no class for today"
         # messages on !roomcheck
-        self.ROOM_UNREG="Room not found. Please try again"
-        self.ROOM_EMPTY="There is no class for in this room for today"
+        self.ROOM_UNREG = "Room not found. Please try again"
+        self.ROOM_EMPTY = "There is no class for in this room for today"
