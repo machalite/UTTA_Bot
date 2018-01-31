2799d52a36c8a710fd19bacec5971cf7eec12d11
machalite <machalite@ymail.com>
2018-02-01 00:44:12 +0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,12 @@
 class Settings:
     def __init__(self):
         # Bot Channel settings
-        self.CHANNEL_TOKEN = 'cnCjBT66E+ViXO8qj7j7Rj30oeHOtrbqo6gqSxxeW++l28tLJbHdGAUPs8a6MnjAZJHAiymHVDrDfs1xCe89VQ+wWY0RrvHfudjn/5OEfx5IaXRGwX/ZkYJl1ouxbBw/4W+HD7ryJDEjjgmSzdbtWwdB04t89/1O/w1cDnyilFU='
-        self.CHANNEL_SECRET = '13235caf7d4824dabbbf2a6393c32dfa'
+        self.CHANNEL_TOKEN = ''
+        self.CHANNEL_SECRET = ''
         # DB connection
-        self.DB_HOST = "us-cdbr-iron-east-05.cleardb.net"
-        self.DB_USER = "bfc1e725797bdf"
-        self.DB_PASS = "c4e344fc"
-        self.DB_NAME = "heroku_2ad6e3d7ef27f71"
+        self.DB_HOST = ""
+        self.DB_USER = ""
+        self.DB_PASS = ""
+        self.DB_NAME = ""
         # set timezone for date and time
         self.TIME_ZONE = 'Asia/Jakarta'
